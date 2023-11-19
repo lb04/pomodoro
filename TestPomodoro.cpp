88d8d9b389925260a4b1ae8cad0eaa71636aec3c
John Bakogiannis <john.bakoyannis@cosmo-one.gr>
2023-11-19 15:56:28 +0200

    fix catch2 version

--- TestPomodoro.cpp
+++ TestPomodoro.cpp
@@ -1,16 +1,14 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#define CATCH_CONFIG_ENABLE_BENCHMARKING 
-#include <catch/catch.hpp>
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
 #include <cstdint>
 #include "PomodoroTimer.h"
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
+TEST_CASE( "Check Pomodoro start session", "[pomodoro]" ) {
     PomodoroTimer timer;
     timer.reset();
     timer.startSession();
     REQUIRE( timer.getTotalWorkDuration() == 1 );
     REQUIRE( timer.getTotalBreakDuration() == 1 );
-    REQUIRE(timer.check_number("1") == true);
-    REQUIRE(timer.validateUserInput("1") == 1);
     
 }
